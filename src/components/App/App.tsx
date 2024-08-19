846ff226ffffbd6e2e95281b0f7b0f7926fae736
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-19 03:13:24 -0400

    add infinite scroll functionality

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,42 @@
 // import { useScrollPosition } from "@/lib/useScrollPosition";
 import ArtifactPhoto from "../ArtifactPhoto";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useScrollEnd } from "@/lib/useScrollEnd";
 import { pexelClient } from "@/lib/pexel-api-client";
+
+type ImageType = { id: number; src: string; alt: string };
 
 function App() {
   // const scrollPosition = useScrollPosition();
   const isScrollEnd = useScrollEnd();
-  // const imageGenRef = useRef(new ImageGenerator());
-  // useEffect(() => {
-  // imageGenRef.current
-  //   .fetchImages()
-  //   .then((res) => console.log(res))
-  //   .catch((err) => console.error(err));
-  // }, [imageGenRef]);
-  useEffect(() => {
-    pexelClient(1)
-      .then((data) => console.log(data))
-      .catch((err) => console.log(`error: ${err.message}`));
-  }, []);
+  const [images, setImages] = useState<ImageType[]>([]);
+  const [pageCount, setPageCount] = useState(1);
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
-    if (isScrollEnd) {
-      alert("End of the line buddy!");
+    if (pageCount === 1 || isScrollEnd) {
+      pexelClient(pageCount).then(async (data) => {
+        try {
+          const { photos } = data;
+          const nextImages = photos.map(({ id, src: { large }, alt }) => ({
+            id,
+            src: large,
+            alt,
+          }));
+          const nextPageCount = pageCount + 1;
+          setImages([...images, ...nextImages]);
+          setPageCount(nextPageCount);
+        } catch (error) {
+          if (error instanceof Error) {
+            console.error(`Error! Source: pexelClient': ${error.message}`);
+          } else {
+            console.error(`Error! Unknown Error:`, error);
+          }
+        }
+      });
     }
-  }, [isScrollEnd]);
+  }, [isScrollEnd, pageCount, images]);
 
   return (
     <main className="container mx-auto">
@@ -32,9 +44,9 @@ function App() {
         <h1 className="mx-4 font-bold mb-10 mt-2">REACT TYPESCRIPT APP</h1>
       </div>
       <div className="flex flex-col items-center space-y-10">
-        <ArtifactPhoto />
-        <ArtifactPhoto />
-        <ArtifactPhoto />
+        {images.map((image) => (
+          <ArtifactPhoto key={image.id} imageSrc={image.src} alt={image.alt} />
+        ))}
       </div>
     </main>
   );
