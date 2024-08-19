decd2649e601c9fef6433a6a28cac374b01190c5
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-19 03:13:39 -0400

    enhance Image component

--- src/components/ArtifactPhoto/ArtifactPhoto.tsx
+++ src/components/ArtifactPhoto/ArtifactPhoto.tsx
@@ -4,16 +4,18 @@ import * as React from "react";
 function ArtifactPhoto({
   imageSrc = "https://images.metmuseum.org/CRDImages/ad/original/112937.jpg",
   className = "",
+  alt = "",
   ...delegatedDivProps
 }: React.ComponentProps<"div"> & {
   imageSrc?: string;
+  alt?: string;
 }) {
   return (
     <div
       className={cn("w-full max-w-[600px] shadow-xl", className)}
       {...delegatedDivProps}
     >
-      <img className="w-full h-auto" src={imageSrc} />
+      <img className="w-full h-auto" src={imageSrc} alt={alt} />
     </div>
   );
 }
