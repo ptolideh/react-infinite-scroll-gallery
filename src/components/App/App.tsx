8489983d2c83e5a37d1fe46bef10c26da168a2eb
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-19 03:37:59 -0400

    add loading spinner

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,18 +3,20 @@ import ArtifactPhoto from "../ArtifactPhoto";
 import { useEffect, useState } from "react";
 import { useScrollEnd } from "@/lib/hooks/useScrollEnd";
 import { pexelClient } from "@/lib/api/pexel-api-client";
+import { LoaderPinwheel } from "lucide-react";
 
 type ImageType = { id: number; src: string; alt: string };
+type StatusType = "idle" | "pending" | "success" | "error";
 
 function App() {
   const isScrollEnd = useScrollEnd();
   const [images, setImages] = useState<ImageType[]>([]);
   const [pageCount, setPageCount] = useState(1);
-
-  useEffect(() => {}, []);
+  const [status, setStatus] = useState<StatusType>("idle");
 
   useEffect(() => {
     if (pageCount === 1 || isScrollEnd) {
+      setStatus("pending");
       pexelClient(pageCount).then(async (data) => {
         try {
           const { photos } = data;
@@ -26,12 +28,14 @@ function App() {
           const nextPageCount = pageCount + 1;
           setImages([...images, ...nextImages]);
           setPageCount(nextPageCount);
+          setStatus("success");
         } catch (error) {
           if (error instanceof Error) {
             console.error(`Error! Source: pexelClient': ${error.message}`);
           } else {
             console.error(`Error! Unknown Error:`, error);
           }
+          setStatus("error");
         }
       });
     }
@@ -40,12 +44,19 @@ function App() {
   return (
     <main className="container mx-auto">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="mx-4 font-bold mb-10 mt-2">REACT TYPESCRIPT APP</h1>
+        <h1 className="mx-4 font-bold mb-10 mt-2">
+          React Infinite Scroll X Pexel Gallery
+        </h1>
       </div>
       <div className="flex flex-col items-center space-y-10">
         {images.map((image) => (
           <ArtifactPhoto key={image.id} imageSrc={image.src} alt={image.alt} />
         ))}
+        {status === "pending" && (
+          <div className="w-full flex items-center justify-center pt-2 pb-10">
+            <LoaderPinwheel size={36} className="animate-spin" />
+          </div>
+        )}
       </div>
     </main>
   );
