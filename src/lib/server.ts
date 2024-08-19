c0f2fd71be3c9ab39d10c0bcf9ea9446c7932ddf
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-19 02:13:16 -0400

    add pexel microservice to fetch images using private api

---
+++ src/lib/server.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.get("/api/pexels", async (req: Request, res: Response) => {
+  try {
+    console.log("Request received for /api/pexels");
+    const apiUrl = process.env.PEXEL_API_URL;
+    if (!apiUrl) {
+      throw new Error("PEXEL_API_URL is not defined in environment variables");
+    }
+
+    const queryParams = new URLSearchParams(
+      req.query as Record<string, string>
+    ).toString();
+    const fullUrl = `${apiUrl}?${queryParams}`;
+    console.log(`Full URL: ${fullUrl}`);
+
+    const apiKey = process.env.PEXEL_API_KEY;
+    if (!apiKey) {
+      throw new Error("PEXEL_API_KEY is not defined in environment variables");
+    }
+
+    const response = await fetch(fullUrl, {
+      headers: {
+        Authorization: `${apiKey}`,
+      },
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error);
+
+    if (error instanceof Error) {
+      res
+        .status(500)
+        .json({ error: error.message || "An unknown error occurred" });
+    } else {
+      res.status(500).json({ error: JSON.stringify(error) });
+    }
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
